020fba6a8a70ac6cc17b23f1db0f9ae07293aaa9
Oscar Cortez <ocortez@fullsail.com>
2015-06-06 11:37:13 -0400

    Initial commit.

---
+++ properties.js
@@ -0,0 +1,14 @@
+var size = 50;
+
+var msg = "Well hello there, friends!";
+
+var colorWords = ["red", "blue", "green"];
+
+var colorHex = ["#205960", "#326B24", "#BF404F", "#D1A975"];
+
+//KEY VALUE PAIRS
+var fontProperties = {
+	color: "#ccc",
+	fontFamily: "Futura-CondensedMedium",
+	fontSize: 18,
+};
